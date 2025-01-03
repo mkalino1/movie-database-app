f5aa00ad59cd59dcb5837fd0b2803ae919977908
mkalino1 <mateuszkalin1@gmail.com>
2025-01-03 22:30:00 +0100

    feat: :sparkles: Add movie rating UI

--- components/MovieDescription.vue
+++ components/MovieDescription.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="mx-auto max-w-[800px]">
+    <div class="flex gap-6">
+      <Icon v-for="i in 5" :name="i <= hoveredIndex ? 'carbon:star-filled' : 'carbon:star'" @click="rate(i)"
+        @mouseover="hoveredIndex = i" @mouseleave="hoveredIndex = -1" size="38" style="color: cadetblue;" />
+    </div>
     <h4 class="my-6 text-3xl">Storyline</h4>
     <p>{{ description }}</p>
     <div class="my-4 border-t-2 border-zinc-700"></div>
@@ -25,5 +29,30 @@
 </template>
 
 <script setup lang="ts">
-defineProps<{ description: string, releaseDate: string, director: string, genre: string, runtime:string }>();
+const props = defineProps<{
+  description: string;
+  releaseDate: string;
+  director: string;
+  genre: string;
+  runtime: string;
+  id: number
+}>();
+
+const hoveredIndex = ref(-1)
+const { loggedIn } = useUserSession()
+
+async function rate(score: number) {
+  if (!loggedIn.value) {
+    await navigateTo('/login')
+    return
+  }
+  try {
+    await $fetch("/api/movies/rating", {
+      method: "POST",
+      body: { movieId: props.id, score: score }
+    });
+  } catch (error) {
+    alert(error.statusMessage || error);
+  }
+};
 </script>
